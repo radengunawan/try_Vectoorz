94277c32dc95c1f9ef207ea684390427dd59699d
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-28 09:06:26 +0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vector vector1 = new Vector(20, 30);
+            Vector vector2 = new Vector(45, 70);
+            Vector vectorResult = new Vector();
+
+            // vectorResult is equal to (65,100)
+            vectorResult = vector1 + vector2;
+
+        }
+    }
+}
---
+++ ConsoleApp2/Vector.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp2
+{
+    internal class Vector
+    {
+        public Vector()
+        {
+        }
+
+        public Vector(int v1, int v2)
+        {
+            this.V1 = v1;
+            this.V2 = v2;
+        }
+
+        public int V1 { get; }
+        public int V2 { get; }
+    }
+}
